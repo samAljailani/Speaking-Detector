78f4f9228284e16b7da4159fc795b7c9e24e373f
sami <saljail@kent.edu>
2022-10-14 17:18:46 -0400

    updated frequency calculator

--- javascript/speakingDetector.js
+++ javascript/speakingDetector.js
@@ -7,13 +7,16 @@ const threshold = 0.003;
 let isTalking = false;
 let currentInterval = 0
 let currentIntervalData = new Array();
+let dataPoints = new Array(20);
+for(let i = 0; i < 20; ++i){
+    dataPoints[i] = [{}, false];
+}
 let speakingFrequency = {
-    points: [],
-    pointsGreaterThanThreshold: 0,
-    totalPointsInInterval : 0,
-    frequencyInterval: 0
-};
-
+    maxNumberOfPoints: 10,
+    points: dataPoints,//[point data, point is greater than thershold]
+    pointsGreaterThanThreshold: 0, 
+    frontOfQueue: 0
+}
 const faceMesh = new FaceMesh({locateFile: (file) => {
     return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
   }});
@@ -21,7 +24,8 @@ const faceMesh = new FaceMesh({locateFile: (file) => {
     maxNumFaces: 1,
     refineLandmarks: true,
     minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
+    minTrackingConfidence: 0.5,
+    selfieMode: true
   });
   faceMesh.onResults(onResults);
   
@@ -41,9 +45,10 @@ function onResults(results) {
     canvasCtx.drawImage(
         results.image, 0, 0, canvasElement.width, canvasElement.height);
     if(isTalking){
-        let percentage = speakingFrequency.pointsGreaterThanThreshold / speakingFrequency.totalPointsInInterval;
-        canvasCtx.rect(10,10, 400 * percentage, 100)
-        canvasCtx.fillStyle = "rgba(75, 115, 195, 0.5)";
+        let percentage = speakingFrequency.pointsGreaterThanThreshold / speakingFrequency.maxNumberOfPoints;
+        console.log(percentage); 
+        canvasCtx.rect(10,10, canvasElement.width * percentage * 0.75, 100)
+        canvasCtx.fillStyle = "rgba(0, 255,255, 0.5)";
         canvasCtx.fill();
     }
     if (results.multiFaceLandmarks) {
@@ -52,15 +57,12 @@ function onResults(results) {
                 if(userIsTalking()){
                     isTalking = true;
                 }else{
+                    speakingFrequency.pointsGreaterThanThreshold = 0;
+                    speakingFrequency.points = dataPoints;
+                    speakingFrequency.frontOfQueue = 0;
                     isTalking = false;
                 }
                 currentInterval = new Date().getTime()+ sampleIntervalDuration;
-
-            }
-            if(new Date().getTime() > speakingFrequency.frequencyInterval){
-                speakingFrequency.pointsGreaterThanThreshold = 0;
-                speakingFrequency.totalPointsInInterval = 0;
-                speakingFrequency.frequencyInterval = new Date().getTime() + frequencyIntervalDuration;
             }
             if(isTalking){
                 canvasCtx.fillStyle = "#000000";
@@ -69,8 +71,7 @@ function onResults(results) {
             }
             currentIntervalData.push(XYZPointDistances(landmarks[13], landmarks[14]));
 
-            drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION,
-                        {color: '#C0C0C070', lineWidth: 1});
+            drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {color: '#C0C0C070', lineWidth: 1});
             drawConnectors(canvasCtx, landmarks, FACEMESH_FACE_OVAL, {color: '#E0E0E0'});
             drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: '#E0E0E0'});
         }
@@ -84,7 +85,7 @@ function userIsTalking(){
     let result = false;
     let newMean          = meanCalculator(currentIntervalData);
     let MAD   = meanAbsoluteDeviation(currentIntervalData, newMean);
-    calculateFrequency(newMean);
+    FrequencyCalculator(newMean);
     for(let i = 0; i < 3; ++i){
         
         if(MAD[i] >=  threshold){
@@ -94,18 +95,29 @@ function userIsTalking(){
     currentIntervalData = [];
     return result;
 }
-function calculateFrequency( mean){
-    let count = 0;
+function FrequencyCalculator(mean){
+    let i = speakingFrequency.frontOfQueue;
+    //insert all elements to speakingFrequency
     for(let point of currentIntervalData){
-            if(    Math.abs(point.x - mean[0]) >= threshold
-                || Math.abs(point.y - mean[1]) >= threshold
-                || Math.abs(point.z - mean[2]) >= threshold){
-                ++speakingFrequency.pointsGreaterThanThreshold;
+        if(    Math.abs(point.x - mean[0]) >= threshold
+            || Math.abs(point.y - mean[1]) >= threshold
+            || Math.abs(point.z - mean[2]) >= threshold){
+
+                if(!speakingFrequency.points[i][1]){
+                    ++speakingFrequency.pointsGreaterThanThreshold;
+                    speakingFrequency.points[i][1] = true;
+                }
+        }else{
+            //if the point was greater than the threshold and the new point is not greater than the threshold
+            // then decrement the number of points greater than the threshold
+            if(speakingFrequency.points[i][1]){
+                --speakingFrequency.pointsGreaterThanThreshold;
             }
-            ++speakingFrequency.totalPointsInInterval;
-        
+            speakingFrequency.points[i][1] = false;
+        }
+        speakingFrequency.points[i][0] = point;
+        i = i + 1 >= speakingFrequency.maxNumberOfPoints? 0: i + 1;
     }
-    return count;
 }
 function meanCalculator(arr){
     let sumX = 0;
