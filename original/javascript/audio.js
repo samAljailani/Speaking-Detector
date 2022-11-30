f0c746e8810ce82ec6afffc99b6cc2017bcade8a
sami <saljail@kent.edu>
2022-11-30 11:55:02 -0500

    made source files unreadable

---
+++ original/javascript/audio.js
@@ -0,0 +1,140 @@
+let audioContext;
+let fftSize = 256 * 2;
+let prevMean= 0, currMean =0;
+let prevMaxFreq=0, currMaxFreq=0;
+let freqSensitivity = 20;
+let size = 2;
+let freqMeanThreshold = 50;
+let currentAudioIntervalData = [];
+console.log(freqMeanThreshold);
+let audioChanged = false;
+let audioRecentlyChanged = false;
+let data;
+/*
+let soundHistory = Array(size);
+//last index is the current mean
+for(let i = 0; i < size; ++i){
+    soundHistory[i] = {mean: 0, maxFreq: 0};
+}
+*/
+
+
+//prompt user for microphone premisison
+if (!navigator.getUserMedia)
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+        navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+if (navigator.getUserMedia) {
+
+    navigator.getUserMedia({ audio: true },
+        function (stream) {
+            audioContext = new (AudioContext || webketAudioContext)();
+           process(stream);
+        },
+        function (e) {
+            alert('Error capturing audio.');
+        }
+    );
+
+} else { alert('getUserMedia not supported in this browser.'); }
+//determine if there is a change in audio every 200 millis
+function audioHasChanged(){
+    
+        let result = false;
+        if(currentAudioIntervalData != []){
+            currMean         = AudioMeanCalculator(currentAudioIntervalData);
+            let currMeanThreshold = freqMeanThreshold * currentAudioIntervalData.length;
+            if(Math.abs(currMean - prevMean) > currMeanThreshold ){
+                result = true;
+            }
+            // console.log(currentAudioIntervalData);
+            // console.log(Math.abs(currMean - prevMean), currMean, prevMean, currMeanThreshold);
+            prevMaxFreq = currMaxFreq;
+            prevMean = currMean;
+            line = true;
+            currentAudioIntervalData = [];
+        }
+    return result;
+    
+    /*data         = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(data); 
+    currMean         = getMean(data);
+    currMaxFreq      = highestFrequency(data);
+    let result = false;
+    //console.log( Math.abs(currMaxFreq - prevMaxFreq), freqSensitivity)
+    if(Math.abs(currMaxFreq - prevMaxFreq) > freqSensitivity ){
+        //Math.abs(currMean - prevMean) > freqMeanThreshold && 
+       result =  true;
+    }
+    prevMaxFreq = currMaxFreq;
+    prevMean = currMean;
+    return result;
+    */
+
+    // if(intervalData != []){
+    //     let result = false;
+    //     currMean         = meanCalculator(intervalData);
+    //     let currMeanThreshold = freqMeanThreshold * intervalData.length;
+    //     if(Math.abs(currMean - prevMean) > currMeanThreshold ){
+    //         result = true;
+    //     }
+    //     prevMaxFreq = currMaxFreq;
+    //     prevMean = currMean;
+    //     line = true;
+    //     intervalData = [];
+    // }
+}
+function getAudioData(){
+    let data         = new Uint8Array(analyser.frequencyBinCount);
+    analyser.getByteFrequencyData(data); 
+    return data;
+}
+let source;
+let filter;
+let analyser;
+let volume;
+let dest;
+function process(stream) {
+    //creating nodes
+    source           = audioContext.createMediaStreamSource(stream);
+    filter           = audioContext.createBiquadFilter(); 
+    analyser         = audioContext.createAnalyser();
+    volume           = audioContext.createGain();
+    dest             = audioContext.createMediaStreamDestination();
+    //config
+    audioContext.fftSize = fftSize;
+    filter.type = "bandpass";
+    filter.Q.value =  0.2;
+    analyser.fftSize = fftSize;
+    volume.gain.value = 1;
+
+    //connect nodes
+    source.connect(volume);
+    volume.connect(filter);
+    filter.connect(analyser);
+    analyser.connect(dest);
+    
+}
+function AudioMeanCalculator(arr){
+    let sum = 0;
+    for(let row = 0; row < arr.length; ++row){
+        arr[row].forEach(i =>{
+            sum += i;
+        });
+    }
+    
+    return sum / arr.length;
+}
+// function highestFrequency(arr){
+//     let max = 0; 
+//     for(let i =0; i < arr.length; ++i){
+//         if(arr[i] != 0){
+//             max = i;
+//         }
+//     }
+//     return max;
+// }
+document.getElementById("sensitivity").value = freqMeanThreshold;
+function changeSensitivity(val){
+    freqMeanThreshold = parseInt(val);
+}
---
+++ original/javascript/speakingDetector.js
@@ -0,0 +1,175 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+const sampleIntervalDuration = 200;
+const cvsExportInterval = 1000; //export row every second
+let row = [];
+let csvData = [];
+let col = 0;
+const threshold = 0.003;
+let mouthWasMoving      = false;
+let mouthIsMoving       = false;
+let startCollectingData = false;
+let currentInterval = 0;
+let currentIntervalData = new Array();
+let dataPoints = new Array(20);
+let isTalking = false;
+let toggleCollection = false;
+for(let i = 0; i < 20; ++i){
+    dataPoints[i] = [{}, false];
+}
+let speakingFrequency = {
+    maxNumberOfPoints: 10,
+    points: dataPoints,//[point data, point is greater than thershold]
+    pointsGreaterThanThreshold: 0, 
+    frontOfQueue: 0
+}
+const faceMesh = new FaceMesh({locateFile: (file) => {
+    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+  }});
+  faceMesh.setOptions({
+    maxNumFaces: 1,
+    refineLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5,
+    selfieMode: true
+  });
+  faceMesh.onResults(onResults);
+  
+  const camera = new Camera(videoElement, {
+    onFrame: async () => {
+      await faceMesh.send({image: videoElement});
+    },
+    width: 1280,
+    height: 720
+  });
+  camera.start();
+
+function onResults(results) {
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
+    
+    if (results.multiFaceLandmarks) {
+        for (const landmarks of results.multiFaceLandmarks) {
+
+            currentIntervalData.push(XYZPointDistances(landmarks[13], landmarks[14]));
+            currentAudioIntervalData.push(getAudioData());
+            drawConnectors(canvasCtx, landmarks, FACEMESH_LIPS, {color: (mouthIsMoving || mouthWasMoving ? '#00FF00': '#E0E0E0')});
+        }
+    }
+    //checking if the user is talking
+    if(new Date().getTime() > currentInterval){
+        if(usermouthIsMoving()){
+            mouthWasMoving = mouthIsMoving;
+            mouthIsMoving = true;
+        }else{
+            mouthWasMoving = mouthIsMoving
+            mouthIsMoving = false;
+        }
+        if(audioHasChanged()){
+            audioRecentlyChanged = audioChanged;
+            audioChanged = true;
+            
+        }else{
+            audioRecentlyChanged = audioChanged;
+            audioChanged = false;
+        }
+        isTalking = (mouthIsMoving || mouthWasMoving) && (audioChanged || audioRecentlyChanged);
+        if(toggleCollection == true && startCollectingData == false){
+            startCollectingData = true;
+        }
+        if(startCollectingData){
+            
+            let date = new Date();
+            row.push(date.toString() +' '+ date.getMilliseconds())
+            row.push(isTalking? "1":"0");
+            if(toggleCollection){
+                row.push(1);
+            }else{
+                row.push(0);
+                startCollectingData = false;
+            }
+            csvData.push(row);
+            row = [];
+        }
+        currentInterval = new Date().getTime()+ sampleIntervalDuration;
+    }
+    //outputing to the canvas whether the client is talking or not
+    canvasCtx.rect(10,10, canvasElement.width * .30, 100);
+    if(isTalking){
+        canvasCtx.fillStyle = "rgba(0, 255,255, 0.7)";//0.7
+        canvasCtx.fill();
+        canvasCtx.fillStyle = "#000000";
+        canvasCtx.font = "40px Verdana";
+        canvasCtx.fillText("Talking", 15, 70);
+    }else{
+        canvasCtx.fillStyle = "rgba(0, 255,255, 0.5)";//0.5
+        canvasCtx.fill();
+        canvasCtx.fillStyle = "#000000";
+        canvasCtx.font = "40px Verdana";
+        canvasCtx.fillText("Not Detected", 15, 70);
+    }
+
+    canvasCtx.restore();
+}
+function distanceBetweenTwoPoints(p1,p2){
+    return Math.sqrt((p1.x - p2.x)**2 + (p1.y - p2.y)**2 , (p1.z - p2.z)**2 )
+} 
+function usermouthIsMoving(){
+    let result = false;
+    let newMean          = meanCalculator(currentIntervalData);
+    let MAD   = meanAbsoluteDeviation(currentIntervalData, newMean);
+    for(let i = 0; i < 3; ++i){
+        
+        if(MAD[i] >=  threshold){
+            result = true;
+        }
+    }
+    currentIntervalData = [];
+    return result;
+}
+function meanCalculator(arr){
+    let sumX = 0;
+    let sumY = 0;
+    let sumZ = 0;
+
+    arr.forEach(i =>{
+        sumX += i.x;
+        sumY += i.y;
+        sumZ += i.z;
+    });
+    return [ sumX / arr.length, sumY / arr.length, sumZ / arr.length];
+}
+function meanAbsoluteDeviation(arr, mean){
+    let sumX = 0;
+    let sumY = 0;
+    let sumZ = 0;
+    arr.forEach(i =>{
+        sumX += Math.abs(i.x - mean[0]);
+        sumY += Math.abs(i.y - mean[1]);
+        sumZ += Math.abs(i.z - mean[2]);
+    });
+    return[
+        sumX/(arr.length),
+        sumY/(arr.length),
+        sumZ/(arr.length)
+    ]
+}
+function XYZPointDistances(p1,p2){
+    return {x: p1.x - p2.x, y: p1.y - p2.y, z: p1.z - p2.z};
+}
+/////////////////
+function exportData() {
+    let csvContent = csvData.map(e => e.join(",")).join("\n");
+
+    var hiddenElement = document.createElement('a');  
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvContent);  
+    hiddenElement.target = '_blank';  
+      
+    //provide the name for the CSV file to be downloaded  
+    hiddenElement.download = 'Talking_Data.csv';  
+    hiddenElement.click();
+  }
